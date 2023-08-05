8b3edc19be1f7600474b073bfa3db18d1c020d0c
Sri Pravan Paturi <srpaturi@amazon.com>
2023-08-05 19:32:34 +0530

    Persist server after multiple pings with defer before loop

--- app/server.go
+++ app/server.go
@@ -7,6 +7,26 @@ import (
 	"strings"
 )
 
+func respond(connection net.Conn) {
+	request_bytes := make([]byte, 16)
+	_, err := connection.Read(request_bytes)
+
+	if err != nil {
+		fmt.Println("Error reading connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	request_string := strings.Trim(string(request_bytes), "\x00")
+
+	if len(request_string) > 0 {
+		fmt.Println("Received PING")
+		connection.Write([]byte("+PONG\r\n"))
+		fmt.Println("Sent PONG")
+	}
+
+	connection.Close()
+}
+
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 
@@ -15,35 +35,25 @@ func main() {
 		os.Exit(1)
 	}
 
+	connection, err := listener.Accept()
+
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	respond(connection)
+
+	defer connection.Close()
+
 	for {
-		connection, err := listener.Accept()
+		connection, err = listener.Accept()
 
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 
-		request_bytes := make([]byte, 16)
-		_, err = connection.Read(request_bytes)
-
-		if err != nil {
-			fmt.Println("Error reading connection: ", err.Error())
-			os.Exit(1)
-		}
-
-		request_string := strings.Trim(string(request_bytes), "\x00")
-
-		if len(request_string) > 0 {
-			fmt.Println("Received PING")
-			connection.Write([]byte("+PONG\r\n"))
-			fmt.Println("Sent PONG")
-		}
-
-		err = connection.Close()
-
-		if err != nil {
-			fmt.Println("Error closing connection: ", err.Error())
-			os.Exit(1)
-		}
+		respond(connection)
 	}
 }
