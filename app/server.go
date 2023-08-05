2b74d5efd694a3f03f9a74abf6da286ea17f7bb5
Sri Pravan Paturi <srpaturi@amazon.com>
2023-08-05 17:34:29 +0530

    Add ping handling

--- app/server.go
+++ app/server.go
@@ -4,17 +4,37 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	connection, err := listener.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	request_bytes := make([]byte, 16)
+	_, err = connection.Read(request_bytes)
+
+	request_string := strings.Trim(strings.TrimSpace(string(request_bytes)), "\n\x00")
+
+	if request_string == "PING" {
+		fmt.Println("Received PING")
+		connection.Write([]byte("+PONG\r\n"))
+		fmt.Println("Sent PONG")
+	}
+
+	if err != nil {
+		fmt.Println("Error reading connection: ", err.Error())
 		os.Exit(1)
 	}
 }
